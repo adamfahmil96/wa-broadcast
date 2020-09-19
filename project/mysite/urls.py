b77138588d38397b2d56992f187b4ff205c05c66
adamfahmil96 <adamfahmil020@gmail.com>
2020-09-19 14:09:23 +0700

    URL Name & Namespace

--- project/mysite/urls.py
+++ project/mysite/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('desa/', include('desa.urls')),
-    path('grup/', include('grup.urls')),
-    path('kontak/', include('kontak.urls')),
-    path('pesan/', include('pesan.urls')),
+    path('', views.index, name="index"),
+    path('desa/', include(('desa.urls', 'desa'), namespace="desa")),
+    path('grup/', include(('grup.urls', 'grup'), namespace="grup")),
+    path('kontak/', include(('kontak.urls', 'kontak'), namespace="kontak")),
+    path('pesan/', include(('pesan.urls', 'pesan'), namespace="pesan")),
     path('admin/', admin.site.urls),
 ]
