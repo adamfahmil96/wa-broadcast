27b1f548410d5e91359d86644d6e252ef525ef1b
adamfahmil96 <adamfahmil020@gmail.com>
2020-09-30 14:09:47 +0700

    Penerapan Class based View

--- project/desa/forms.py
+++ project/desa/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 
-class DesaForm(forms.Form):
-    name = forms.CharField(
-        label       = 'Nama Desa',
-        max_length  = 100,
-        widget      = forms.TextInput(
-            attrs   = {
-                'class': 'form-control',
-                'placeholder': 'Masukkan nama desa',
-            }
-        )
-    )
+from .models import Desa
+
+class DesaForm(forms.ModelForm):
+    class Meta:
+        model   = Desa
+        fields  = ['name']
+        widgets = {
+            'name': forms.TextInput(
+                attrs   = {
+                    'class': 'form-control',
+                    'placeholder': 'Masukkan nama desa',
+                }
+            )
+        }
--- project/desa/views.py
+++ project/desa/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 from .models import Desa
 from .forms import DesaForm
+
+class DesaListView(TemplateView):
+    template_name   = ''
 
 def index(request):
     desas = reversed(Desa.objects.all().order_by('id'))
@@ -15,15 +19,20 @@ def index(request):
     return render(request, 'desa/index.html', context)
 
 def tambah(request):
-    desa_form = DesaForm()
+    desa_form = DesaForm(request.POST or None)
+    error = None
+    valid = False
     context = {
         'Judul': 'Tambah Desa',
         'Subjudul': 'Masukkan Desa Baru',
         'desa_form': desa_form,
+        'error': error,
     }
     if request.method == 'POST':
-        Desa.objects.create(
-            name = request.POST.get('name'),
-        )
-        return HttpResponseRedirect("/desa")
+        if desa_form.is_valid():
+            desa_form.save()
+            return redirect('desa:index')
+        else:
+            error = desa_form.errors
+            context['error'] = error
     return render(request, 'desa/tambah.html', context)
